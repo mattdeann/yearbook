cf7a528c12d4cf5abdfce6f6838f7362d55a010d
mattdean <matthewallandean@gmail.com>
2021-02-01 18:27:04 -0700

    Add grid structure for data

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -3,8 +3,27 @@ import Person from '../Person/Person';
 import './Cohort.css';
 
 
-function Cohort() {
+function Cohort(props) {
 
+  return (
+    <>
+      <h1>
+        Staff
+      </h1>
+      <section className="cohort">
+        {props.data.map(person => {
+          return (
+            <div>
+              <p>{person.name}</p>
+              <img className="person-image" src={person.photo} alt="personal"/>
+              <p>{person.quote}</p>
+              <p>{person.superlative}</p>
+            </div>
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
 export default Cohort;
