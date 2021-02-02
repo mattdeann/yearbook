8c651bd1dbc55333df7e8ef69bbb0e8ee8feeac8
mattdean <matthewallandean@gmail.com>
2021-02-01 18:04:42 -0700

    Fix errors

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Person from '../Person/Person';
 import './Cohort.css';
 
-// COHORT COMPONENT CODE GOES HERE
+
+function Cohort() {
+
+}
 
 export default Cohort;
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,5 +2,8 @@ import React from 'react';
 import './Person.css';
 
 // PERSON COMPONENT CODE GOES HERE
+function Person() {
+
+}
 
 export default Person;
